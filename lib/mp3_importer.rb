1ca41edf2952ace0b0063b486851fe854f772437
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-11 19:55:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -15,7 +15,7 @@ class MP3Importer
 
   # imports the files into the library by creating songs from a filename
   def import
-    files.eachSong.new_by_filename(files)
+    files.each {|song| Song.new_by_filename(song)}
 
   end
 
