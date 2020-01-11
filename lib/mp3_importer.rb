c3bde808ae422b36b23902a2161a4cdc0d3a8ea8
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-11 19:41:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
   # normalizes the filename to just the mp3 filename with no path
   def files
     # binding.pry
-    Dir.entries(@path).select { |f| File.file?(f) }
+    Dir.entries(@path).select { |f| f.file? }
   end
 
   def import
